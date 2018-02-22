e09fed188361ce8811f69330e27e04e31679c7fe
Donnadieu <donnadieu.86@gmail.com>
2018-02-22 14:57:59 -0500

    Updated specs

--- spec/models/coin_spec.rb
+++ spec/models/coin_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Coin, type: :model do
   before do
-    @wallet = create(:wallet)
+    @user = create(:user)
+    @wallet = create(:wallet, user_id: @user.id)
   end
   it 'has a valid "Coin" factory' do
     expect(build(:coin, wallet_id: @wallet.id)).to be_valid
@@ -26,10 +27,10 @@ RSpec.describe Coin, type: :model do
     expect(coin.difficulty).not_to be_nil
   end
 
-  it 'has a price' do
+  it 'has an amount' do
     coin = build(:coin, wallet_id: @wallet.id)
 
-    expect(coin.price).not_to be_nil
+    expect(coin.amount).not_to be_nil
   end
 
   describe 'Assosciation with Wallet' do
