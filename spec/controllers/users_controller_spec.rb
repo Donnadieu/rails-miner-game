2c36082dc1bd3e1dd260b97d5ba872a6a1fad5ce
Donnadieu <donnadieu.86@gmail.com>
2018-02-23 14:59:21 -0500

    Updated users controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,16 +2,20 @@ require 'rails_helper'
 
 describe UsersController, type: :controller do
   render_views
-  context 'logged in' do
-    it 'should be redirected to signin when not logged in' do
-      login_with nil
-      get :index
-      expect(response).to redirect_to(new_user_session_path)
-    end
-    it 'should let a user see homepage when logged in' do
-      login_with create(:user)
-      get :home
-      expect(response).to render_template(:home)
+  require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+  describe 'GET #index' do
+    context 'logged in' do
+      it 'should be redirected to signin when not logged in' do
+        login_with nil
+        get :index
+        expect(response).to redirect_to(new_user_session_path)
+      end
+      it 'should let a user see homepage when logged in' do
+        login_with create(:user)
+        get :home
+        expect(response).to render_template(:home)
+      end
     end
   end
 end
