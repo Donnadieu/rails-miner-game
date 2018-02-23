bb93ea0b64c6792905e02f0614f55eab534f3aa4
Donnadieu <donnadieu.86@gmail.com>
2018-02-22 19:58:27 -0500

    added tests for Users controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  let(:valid_attributes) {
+    {
+      username: 'omar',
+      email: 'omar@email.com',
+      password: '12345678'
+    }
+  }
+  let(:valid_session) { {} }
 
+  describe 'GET index' do
+    it 'assigns all users as @users' do
+      user = User.create! valid_attributes
+
+      get :index, params: {}, session: valid_session
+
+      expect(assigns(:users)).to eq([user])
+    end
+  end
 end
