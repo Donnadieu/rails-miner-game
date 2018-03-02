6b9d34e8b154d8cd0992b867516d0c1f50e74e4a
Donnadieu <donnadieu.86@gmail.com>
2018-03-02 14:00:13 -0500

    Added footer and icons

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module RailsMinerGame
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.active_job.queue_adapter = :delayed_job
+    config.assets.paths << Rails.root.join('/vendor/assets/fonts')
+    config.assets.enabled = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
