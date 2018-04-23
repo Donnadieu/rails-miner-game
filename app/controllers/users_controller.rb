2c0870850659cb889f8634204ed74a38b94a4b75
Donnadieu <donnadieu.86@gmail.com>
2018-04-23 13:37:06 -0400

    setting up serializer Associations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,23 @@
 class UsersController < ApplicationController
-
+  before_action :set_user, only: [:show]
   def index
     redirect_to user_path(current_user)
   end
 
   def show
+    respond_to do |format|
+      format.html { render 'show'}
+      format.json { render json: @user, status: 200}
+    end
   end
 
   def highest_balance
     @users = User.highest_balance
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
--- app/serializers/mining_rig_serializer.rb
+++ app/serializers/mining_rig_serializer.rb
@@ -1,5 +1,5 @@
 class MiningRigSerializer < ActiveModel::Serializer
   attributes :id, :name, :status
   has_many :miners
-
+  belongs_to :user
 end
