f058e5dbff411ecc406ec1f830a24d220daf9958
Donnadieu <donnadieu.86@gmail.com>
2018-04-23 20:37:02 -0400

    render index page through ajax request

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -51,7 +51,7 @@ $(document).ready(function() {
   $("#mining-rigs-button, #mining-rigs-header ").unbind("click").click(function(event) {
     event.preventDefault()
     var userId = event.target.dataset.id
-    var url = `/users/${id}/mining_rigs.json`
+    var url = `/users/${userId}/mining_rigs.json`
 
     $.get(url, function(response){
       $("#main").html(renderMiningRigIndex(userId))
